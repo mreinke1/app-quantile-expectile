298f26ed9610127015af8530a0e961ebea84dac3
Martin <martin_reinke@gmx.de>
2022-01-12 15:01:05 +0100

    Update app-quantile-expectile.py

--- app-quantile-expectile.py
+++ app-quantile-expectile.py
@@ -156,7 +156,8 @@ with row2_1:
     st.write("Our approach (BIRS)")
     base = alt.Chart(source_data).mark_circle(clip = True, color = '#7D3C98').encode(
         alt.X("K/F", scale=alt.Scale(domain=[0.7,1.2])),
-        alt.Y("callprice", scale=alt.Scale(domain=[0, 200]), legend=alt.Legend(title='teest'))
+        alt.Y("callprice", scale=alt.Scale(domain=[0, 200]),
+              tooltip=['a', 'b', 'c'])
 )
     c = alt.Chart(source_birs[['K/F','prices']]).mark_line(clip=True).encode(
         alt.X('K/F', scale=alt.Scale(domain=[0.7,1.2]), axis=alt.Axis(title='Forward moneyness K/F')), 
