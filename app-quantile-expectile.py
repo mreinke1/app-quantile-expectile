0a831c47b9a2cda005cb23e7e715490d83e592d7
Martin <martin_reinke@gmx.de>
2022-01-12 15:17:57 +0100

    Update app-quantile-expectile.py

--- app-quantile-expectile.py
+++ app-quantile-expectile.py
@@ -157,7 +157,7 @@ with row2_1:
     base = alt.Chart(source_data).mark_circle(clip = True, color = '#7D3C98').encode(
         alt.X("K/F", scale=alt.Scale(domain=[0.7,1.2]), title=''),
         alt.Y("callprice", scale=alt.Scale(domain=[0, 200]), title=''),
-        alt.Color(legend=alt.Legend(values=['Observed market prices'])),
+        alt.Color('symbol',scale=alt.Scale(range=['lightgray'], domain=['AAPL']))
         
 )
     c = alt.Chart(source_birs[['K/F','prices']]).mark_line(clip=True).encode(
