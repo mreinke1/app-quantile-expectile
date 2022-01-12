43e37d1a230bd31e3b184dd6a26c0e7d3eb2c283
Martin <martin_reinke@gmx.de>
2022-01-12 13:58:19 +0100

    Update app-quantile-expectile.py

--- app-quantile-expectile.py
+++ app-quantile-expectile.py
@@ -165,7 +165,7 @@ with row2_1:
     
     #st.altair_chart(c, use_container_width=True)
     
-    alt.layer(base, c)
+    st.altair_chart(base, use_container_width=True)
 
 with row2_2:
     st.write("Quantile-CDF")
