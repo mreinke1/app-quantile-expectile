fd81f74eef89b41f598996c7b3f4602af2e51e2d
Martin <martin_reinke@gmx.de>
2022-01-12 15:14:47 +0100

    Update app-quantile-expectile.py

--- app-quantile-expectile.py
+++ app-quantile-expectile.py
@@ -155,8 +155,10 @@ source_bondarenko = get_dataset(groupList_bondarenko, date_selected, 'bondarenko
 with row2_1:
     st.write("Our approach (BIRS)")
     base = alt.Chart(source_data).mark_circle(clip = True, color = '#7D3C98').encode(
-        alt.X("K/F", scale=alt.Scale(domain=[0.7,1.2]), legend=alt.Legend(values=['Total'])),
+        alt.X("K/F", scale=alt.Scale(domain=[0.7,1.2])),
         alt.Y("callprice", scale=alt.Scale(domain=[0, 200])),
+        alt.Color(legend=alt.Legend(values=['Total'])),
+        
 )
     c = alt.Chart(source_birs[['K/F','prices']]).mark_line(clip=True).encode(
         alt.X('K/F', scale=alt.Scale(domain=[0.7,1.2]), axis=alt.Axis(title='Forward moneyness K/F')), 
